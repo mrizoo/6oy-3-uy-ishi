7d17204ec8aa895181eca36e0bfb0b6b6a8a1421
mukhammadrizo <pulatovvmukhammadrizo@gmail.com>
2024-04-20 03:19:31 +0500

    react task

--- src/App.js
+++ src/App.js
@@ -1,23 +1,131 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import allProducts from "./data (4)";
 function App() {
+  const [products, setProducts] = useState(allProducts);
+  console.log(products);
+
+  //delete product
+
+  const deleteProduct = (id) => {
+    const filterProducts = products.filter((product) => {
+      return product.id !== id;
+    });
+    setProducts(filterProducts);
+  };
+
+  // filter container
+  const filterByBrand = (brand) => {
+    setProducts(allProducts);
+    if (brand == "all") {
+      setProducts(allProducts);
+    } else {
+      const filterBrand = allProducts.filter((product) => {
+        return product.brand == brand;
+      });
+      setProducts(filterBrand);
+    }
+  };
+  //  ================================
+  // rating
+  const filterByRating = (rating) => {
+    if (rating === "all") {
+      setProducts(allProducts);
+    } else {
+      const minRating = parseInt(rating);
+      const maxRating = minRating + 0.2;
+      const filterRate = allProducts.filter((product) => {
+        return product.rating >= minRating && product.rating < maxRating;
+      });
+      setProducts(filterRate);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <div className="filter_container">
+        <select
+          className="input"
+          onChange={(e) => filterByBrand(e.target.value)}
         >
-          Learn React
-        </a>
-      </header>
+          <option value="all">All</option>
+          {[
+            ...new Set(
+              allProducts.map((product) => {
+                return product.brand;
+              })
+            ),
+          ].map((brand) => {
+            return (
+              <option key={Math.random() * new Date()} value={brand}>
+                {brand}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+      {/* ============================================ */}
+      <div className="rating_container">
+        <select
+          className="input"
+          onChange={(e) => filterByRating(e.target.value)}
+        >
+          <option value="all">rating</option>
+          {[
+            ...new Set(
+              allProducts.map((product) => {
+                return product.rating;
+              })
+            ),
+          ].map((rating) => {
+            return (
+              <option key={Math.random() * new Date()} value={rating}>
+                {rating}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+
+      <ul className="product_grid">
+        {products.map((product) => {
+          const {
+            id,
+            title,
+            brand,
+            description,
+            price,
+            discountPercentage,
+            thumbnail,
+            rating,
+          } = product;
+          return (
+            <li key={id}>
+              <img src={thumbnail} alt="" width={400} />
+              <div className="card_body">
+                <h2>{title}</h2>
+                <p>
+                  <b>Brand</b>: {brand}
+                </p>
+                <p>
+                  <b>Description</b>: {description}
+                </p>
+                <p>
+                  <b>Price</b>: $ {price}
+                </p>
+                <p>
+                  <b>Discount</b>: {discountPercentage}%
+                </p>
+                <p>
+                  <b>Rating</b>: {rating}
+                </p>
+              </div>
+              <button className="btn" onClick={() => deleteProduct(id)}>
+                <a>Delete</a>
+              </button>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
